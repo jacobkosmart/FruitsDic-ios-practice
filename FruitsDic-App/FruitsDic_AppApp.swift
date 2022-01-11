4fb9c9ceb1ca42a95b2af92bef1c1cafd8e32586
jacobkosmart <jacobkosmart@gmail.com>
2022-01-11 11:08:48 +0900

    Initial Commit

---
+++ FruitsDic-App/FruitsDic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsDic_AppApp.swift
+//  FruitsDic-App
+//
+//  Created by Jacob Ko on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct FruitsDic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
